996df0d25677dfcede7d7adc88cd67c3e60481a5
Radoslav Velichkov <rado88x@gmail.com>
2022-02-21 19:18:04 +0200

    split slices in separate files

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,7 +1,7 @@
-import { useSelector, useDispatch } from "react-redux";
+import { useDispatch } from "react-redux";
 
 import classes from "./Auth.module.css";
-import { authActions } from "../store/index";
+import { authActions } from "../store/auth";
 
 const Auth = () => {
   const dispatch = useDispatch();
--- src/store/index.js
+++ src/store/index.js
@@ -1,89 +1,10 @@
-import { createSlice, configureStore } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 
-const initialCounterState = { counter: 0, showCounter: true };
-
-const counterSlice = createSlice({
-  name: "counter",
-  initialState: initialCounterState,
-  reducers: {
-    increment(state) {
-      //INTERNALY it will create copy of state, so mutating the state is not problem anymore
-      state.counter++;
-    },
-    decrement(state) {
-      state.counter--;
-    },
-    increase(state, action) {
-      state.counter = state.counter + action.payload;
-    },
-    toggleCounter(state) {
-      state.showCounter = !state.showCounter;
-    },
-  },
-});
-
-const initialAuthState = {
-  isAuthenticated: false,
-};
-
-const authSlice = createSlice({
-  name: "authentication",
-  initialState: initialAuthState,
-  reducers: {
-    login(state) {
-      state.isAuthenticated = true;
-    },
-    logout(state) {
-      state.isAuthenticated = false;
-    },
-  },
-});
+import counterReducer from "./counter";
+import authReducer from "./auth";
 
 const store = configureStore({
-  reducer: { counter: counterSlice.reducer, auth: authSlice.reducer },
+  reducer: { counter: counterReducer, auth: authReducer },
 });
 
-export const counterActions = counterSlice.actions;
-export const authActions = authSlice.actions;
-
 export default store;
-
-// const counterReducer = (state = initialState, action) => {
-//   if (action.type === "increment") {
-//     // state.counter++; //WRONG
-//     //NEVER update state! Always replace state with brand new object return {}
-
-//     return {
-//       //CORRECT
-//       counter: state.counter + 1,
-//       showCounter: state.showCounter,
-//     };
-//   }
-
-//   if (action.type === "increase") {
-//     return {
-//       counter: state.counter + action.value,
-//       showCounter: state.showCounter,
-//     };
-//   }
-
-//   if (action.type === "decrement") {
-//     return {
-//       counter: state.counter - 1,
-//       showCounter: state.showCounter,
-//     };
-//   }
-
-//   if (action.type === "toggle") {
-//     return {
-//       showCounter: !state.showCounter,
-//       counter: state.counter,
-//     };
-//   }
-
-//   return state;
-// };
-
-// const store = createStore(counterReducer);
-
-// export default store;
